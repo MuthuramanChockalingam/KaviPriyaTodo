e46f3bc079750aacbaac1bf2bc4461f6f81c5759
MuthuramanChockalingam <ramsmart710@gmail.com>
2023-04-28 19:05:25 +0530

    Converted Todo from Functional to Class component.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,17 @@
-const Todo = ({todo}) =>
-    <div>
-        <span>{todo.id}</span>
-        <span>{todo.content}</span>
-        <input type="checkBox" checked={todo.status}></input>
-    </div>;
+import { Component } from "react";
+
+class Todo extends Component {
+    render() {
+        const {todo} = this.props;
+
+        return (
+            <div>
+                <span>{todo.id}</span>
+                <span>{todo.content}</span>
+                <input type="checkBox" checked={todo.status}></input>
+            </div>
+        )
+    }
+}
 
 export default Todo;
